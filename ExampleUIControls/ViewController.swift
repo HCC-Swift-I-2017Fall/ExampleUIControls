74ac4a5038c28738ea65b26d0778616bbd323a53
psachania <prakash.sachania@in.ibm.com@prakashs-macbook-pro15.local>
2017-10-18 19:34:00 -0500

    Original version

--- ExampleUIControls/ViewController.swift
+++ ExampleUIControls/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startAgainButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var sentenceTextField: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var appendSwitch: UISwitch!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        displayLabel.text = "To start, enter some text in the text field above and press return. You can then use switch and slider"
+        
+        fontSizeSlider.value = Float(displayLabel.font.pointSize)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func donePressed(_ sender: UIButton) {
+        appendReplaceText()
     }
-
-
+    
+    @IBAction func startAgainPressed(_ sender: UIButton) {
+        displayLabel.text = ""
+        sentenceTextField.text = ""
+    }
+    
+    //Return key did not work
+    @IBAction func sentenceEntered(_ sender: UITextField) {
+        appendReplaceText()
+    }
+    
+    @IBAction func appendSwitchSelected(_ sender: UISwitch) {
+    }
+    
+    @IBAction func fontSizeChanging(_ sender: UISlider) {
+        
+        displayLabel.font = UIFont(name: displayLabel.font.fontName, size: CGFloat(sender.value.rounded()))
+                
+    }
+    
+    func appendReplaceText() {
+        if !appendSwitch.isOn {
+            displayLabel.text = ""
+        }
+        
+        if sentenceTextField.text == nil {
+            sentenceTextField.text = ""
+        }
+        
+        displayLabel.text = displayLabel.text! + "\n" + sentenceTextField.text!
+    }
+    
 }
 
